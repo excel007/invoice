740a3176948391be874e4a2cb928c87f81bc475d
excel007 <eak.ekkachai@gmail.com>
2025-01-03 15:33:43 +0700

    commit

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,15 +1,20 @@
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function page() {
+export default async function page() {
+    const results = await db.execute(sql`Select current_database()`);
     return (
-        <div className="flex flex-col gap-6  mx-auto px-4">
-            <h1 className="text-5xl font-bold">
+        <div className="flex flex-col items-center  px-4">
+            <h1 className="text-5xl font-bold mb-6">
                 Create Invoice
             </h1>
-            <form className="grid gap-4 max-w-xs">
+            {JSON.stringify(results)}
+            <form className="grid gap-4 max-w-xs w-full p-6 bg-white shadow-lg rounded-lg">
                 <Label className="block">Billing Name</Label>
                 <Input type="text" id="name" placeholder="Fullname" />
                 <Label className="block">Billing Email</Label>
